11b21f25f48b45c0c734941714f4f826b4b2f556
Thomas Morel <leromt@gmail.com>
2019-05-16 18:14:20 -0400

    Completed BookDetail Scene and split app into multiple storyboards

---
+++ ReadingList/BookDetailController.swift
@@ -0,0 +1,20 @@
+//
+//  BookDetailController.swift
+//  ReadingList
+//
+//  Created by Thomas Morel on 5/16/19.
+//  Copyright © 2019 Thomas Morel. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailController: UITableViewController
+{
+
+    @IBAction func cancel(unwindSeque: UIStoryboardSegue){
+
+
+
+    }
+
+}
---
+++ ReadingList/ReadingListController.swift
@@ -0,0 +1,21 @@
+//
+//  ReadingListController.swift
+//  ReadingList
+//
+//  Created by Thomas Morel on 5/16/19.
+//  Copyright © 2019 Thomas Morel. All rights reserved.
+//
+import UIKit
+
+class ReadingListController: UITableViewController
+{
+    @IBAction func cancel(unwindSeque: UIStoryboardSegue){
+
+        
+
+    }
+
+    @IBAction func done(unwindSeque: UIStoryboardSegue){
+
+    }
+}
